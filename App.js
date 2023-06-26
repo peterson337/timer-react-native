1eb47f567c8ec2dc5070c4d430ea630a3665e1bf
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2023-06-26 15:59:49 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,180 @@
+import React, {useState, useRef} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 export default function App() {
+        const [segundos, setarSegundos] = useState(0);
+        const [minutos, setarMinutos] = useState(0);
+        const [alarme, setarAlarme] = useState([
+          {
+            id: 1,
+            selecionado: true,
+            som: 'alarme 1',
+            file: '',
+          },
+
+          {
+            id: 2,
+            selecionado: false,
+            som: 'alarme 2',
+            file: '',
+          }
+        ]);
+        const [selectedLanguage, setSelectedLanguage] = useState();
+
+        const pickerRef = useRef();
+
+        function open() {
+          pickerRef.current.focus();
+        }
+
+        function close() {
+          pickerRef.current.blur();
+        }
+
+        var numeros = [];
+        for (var i = 1; i < 60; i++) {
+          numeros.push(i);
+          
+        }
+
+        const setarAlarmeSound = (id) => {
+            let alarmesTemp = alarme.map((val) => {
+                      if (id != val.id) 
+                        val.selecionado = false;
+                        
+                      else
+                        val.selecionado = true;
+                        return val;
+
+                      
+            })
+                setarAlarme(alarmesTemp);
+        }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden />
+
+      <LinearGradient
+        // Background Linear Gradient
+        colors={['rgba(0, 84, 163,0.4)', 'rgba(0, 84, 163,0.9)']}
+        style={styles.background}
+      />
+
+      <Text
+      style={styles.Text}
+      >
+      Selecione o seu tempo:
+      </Text>
+
+      <View
+       style={{flexDirection: 'row'}} 
+      >
+
+       <Text
+       style={{...styles.Text, 
+                  fontSize: 15,
+               }}
+       >Min:
+       </Text> 
+
+      <Picker
+        selectedValue={minutos}
+        onValueChange={(itemValue, itemIndex) =>
+          setarMinutos(itemValue)
+        }
+        style={styles.picker}
+        >
+            <Picker.Item label='0' value='0' />
+
+        {
+          numeros.map((val) => {
+            return(
+            <Picker.Item
+             key={i}
+             label={val.toString()}
+             value={val.toString()}
+             />
+            )
+          })
+        }
+      </Picker>
+
+       <Text
+        style={{...styles.Text, 
+          fontSize: 15, 
+        }}
+       >
+        Seg:
+        </Text> 
+
+      <Picker
+       selectedValue={segundos}
+       onValueChange={(itemValue, itemIndex) =>
+         setarSegundos(itemValue)
+       }
+        style={styles.picker}
+        >
+       {
+          numeros.map((val) => {
+            return(
+            <Picker.Item
+             key={i}
+             label={val.toString()}
+             value={val.toString()}
+             />
+            )
+          })
+        }
+      </Picker>
+      </View>
+
+      <View
+      style={{flexDirection: 'row'}}
+      >
+       {
+        alarme.map((val) => {
+          if (val.selecionado) {
+            return(
+              <TouchableOpacity
+              onPress={() => setarAlarmeSound(val.id)}
+              key={val.som}
+              style={styles.btnEscolherSelecionado}
+             >
+               <Text
+              style={styles.btnEscolherText}
+     
+               > 
+               {val.som}
+               </Text>
+     
+             </TouchableOpacity>
+            )  
+          }else{
+            return(
+              <TouchableOpacity
+              onPress={() => setarAlarmeSound(val.id)}
+              key={val.som}
+              style={styles.btnEscolher}
+             >
+               <Text
+              style={styles.btnEscolherText}
+     
+               > 
+               {val.som}
+               </Text>
+     
+             </TouchableOpacity>
+            )
+          }
+          
+        })
+       }
+      </View>
     </View>
   );
 }
@@ -13,8 +182,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    //backgroundColor: '#349eeb',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  Text: {
+    color: 'white',
+    fontSize: 30,
+    paddingTop: 16,
+  },
+
+  picker: {
+    height: 50,
+    width: 100,
+    color: 'white',
+  },
+
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: "100%",
+  },
+
+  btnEscolher: {
+    padding: 8,
+    backgroundColor: '#1b79d1',
+    borderRadius: 20,
+    marginRight: 10,
+  },
+
+  btnEscolherText: {
+    color: 'white',
+  },
+
+  btnEscolherSelecionado: {
+    padding: 8,
+    backgroundColor: 'rgba(209, 42, 27, 0.8)',
+    borderRadius: 20,
+    marginRight: 10,
+    borderColor: 'white',
+    borderWidth: 1,
+  },
+
 });
