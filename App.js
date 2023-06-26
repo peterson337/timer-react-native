f108f0ea221ab264cbaffff97f36f2021f178d35
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2023-06-26 19:03:11 -0300

    Projeto em desenvolvimento

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import { LinearGradient } from 'expo-linear-gradient';
 export default function App() {
         const [segundos, setarSegundos] = useState(0);
         const [minutos, setarMinutos] = useState(0);
+        const [estado, setarEstado] = useState('selecionar');
         const [alarme, setarAlarme] = useState([
           {
             id: 1,
@@ -55,131 +56,174 @@ export default function App() {
                 setarAlarme(alarmesTemp);
         }
 
-  return (
-    <View style={styles.container}>
-      <StatusBar hidden />
-
-      <LinearGradient
-        // Background Linear Gradient
-        colors={['rgba(0, 84, 163,0.4)', 'rgba(0, 84, 163,0.9)']}
-        style={styles.background}
-      />
-
-      <Text
-      style={styles.Text}
-      >
-      Selecione o seu tempo:
-      </Text>
-
-      <View
-       style={{flexDirection: 'row'}} 
-      >
-
-       <Text
-       style={{...styles.Text, 
-                  fontSize: 15,
-               }}
-       >Min:
-       </Text> 
-
-      <Picker
-        selectedValue={minutos}
-        onValueChange={(itemValue, itemIndex) =>
-          setarMinutos(itemValue)
-        }
-        style={styles.picker}
-        >
-            <Picker.Item label='0' value='0' />
-
-        {
-          numeros.map((val) => {
+          if (estado == 'selecionar') {
             return(
-            <Picker.Item
-             key={i}
-             label={val.toString()}
-             value={val.toString()}
-             />
-            )
-          })
-        }
-      </Picker>
+                <View style={styles.container}>
+                  <StatusBar hidden />
+            
+                  <LinearGradient
+                    // Background Linear Gradient
+                    colors={['rgba(0, 84, 163,0.4)', 'rgba(0, 84, 163,0.9)']}
+                    style={styles.background}
+                  />
+            
+                  <Text
+                  style={styles.Text}
+                  >
+                  Selecione o seu tempo:
+                  </Text>
+            
+                  <View
+                   style={{flexDirection: 'row'}} 
+                  >
+            
+                   <Text
+                   style={{...styles.Text, 
+                              fontSize: 15,
+                           }}
+                   >Min:
+                   </Text> 
+            
+                  <Picker
+                    selectedValue={minutos}
+                    onValueChange={(itemValue, itemIndex) =>
+                      setarMinutos(itemValue)
+                    }
+                    style={styles.picker}
+                    >
+                        <Picker.Item label='0' value='0' />
+            
+                    {
+                      numeros.map((val) => {
+                        return(
+                        <Picker.Item
+                         key={i}
+                         label={val.toString()}
+                         value={val.toString()}
+                         />
+                        )
+                      })
+                    }
+                  </Picker>
+            
+                   <Text
+                    style={{...styles.Text, 
+                      fontSize: 15, 
+                    }}
+                   >
+                    Seg:
+                    </Text> 
+            
+                  <Picker
+                   selectedValue={segundos}
+                   onValueChange={(itemValue, itemIndex) =>
+                     setarSegundos(itemValue)
+                   }
+                    style={styles.picker}
+                    >
+                   {
+                      numeros.map((val) => {
+                        return(
+                        <Picker.Item
+                         key={i}
+                         label={val.toString()}
+                         value={val.toString()}
+                         />
+                        )
+                      })
+                    }
+                  </Picker>
+                  </View>
+            
+                  <View
+                  style={{flexDirection: 'row'}}
+                  >
+                   {
+                    alarme.map((val) => {
+                      if (val.selecionado) {
+                        return(
+                          <TouchableOpacity
+                          onPress={() => setarAlarmeSound(val.id)}
+                          key={val.som}
+                          style={styles.btnEscolherSelecionado}
+                         >
+                           <Text
+                          style={styles.btnEscolherText}
+                 
+                           > 
+                           {val.som}
+                           </Text>
+                 
+                         </TouchableOpacity>
+                        )  
+                      }else{
+                        return(
+                          <TouchableOpacity
+                          onPress={() => setarAlarmeSound(val.id)}
+                          key={val.som}
+                          style={styles.btnEscolher}
+                         >
+                           <Text
+                          style={styles.btnEscolherText}
+                 
+                           > 
+                           {val.som}
+                           </Text>
+                 
+                         </TouchableOpacity>
+                        )
+                      }
+                      
+                    })
+                   }
+                  </View>
 
-       <Text
-        style={{...styles.Text, 
-          fontSize: 15, 
-        }}
-       >
-        Seg:
-        </Text> 
+                  <TouchableOpacity
+                  style={styles.btnIniciar}
+                  onPress={() => setarEstado('iniciar')}
+                  >
+                    <Text
+                    style={styles.btnIniciarText}
+                    >
+                      Iniciar
+                    </Text>
 
-      <Picker
-       selectedValue={segundos}
-       onValueChange={(itemValue, itemIndex) =>
-         setarSegundos(itemValue)
-       }
-        style={styles.picker}
-        >
-       {
-          numeros.map((val) => {
-            return(
-            <Picker.Item
-             key={i}
-             label={val.toString()}
-             value={val.toString()}
-             />
-            )
-          })
-        }
-      </Picker>
-      </View>
-
-      <View
-      style={{flexDirection: 'row'}}
-      >
-       {
-        alarme.map((val) => {
-          if (val.selecionado) {
-            return(
-              <TouchableOpacity
-              onPress={() => setarAlarmeSound(val.id)}
-              key={val.som}
-              style={styles.btnEscolherSelecionado}
-             >
-               <Text
-              style={styles.btnEscolherText}
-     
-               > 
-               {val.som}
-               </Text>
-     
-             </TouchableOpacity>
-            )  
-          }else{
-            return(
-              <TouchableOpacity
-              onPress={() => setarAlarmeSound(val.id)}
-              key={val.som}
-              style={styles.btnEscolher}
-             >
-               <Text
-              style={styles.btnEscolherText}
-     
-               > 
-               {val.som}
-               </Text>
-     
-             </TouchableOpacity>
-            )
+                  </TouchableOpacity>
+                </View>
+              );
+            
+          } else if(estado == 'iniciar'){
+                return(
+                  <View>
+                    <Text>Começou</Text>
+                  </View>
+                )
           }
-          
-        })
-       }
-      </View>
-    </View>
-  );
 }
 
 const styles = StyleSheet.create({
+
+  btnIniciar: {
+      backgroundColor: '#0f35a6',
+      width: 100,
+      height: 100,
+      borderRadius: 50,
+      marginTop: 30,
+      borderColor: 'white',
+      borderWidth: 2,
+  
+  },
+
+  btnIniciarText: {
+       position: 'relative',
+       padding: 30,
+       color: 'white',
+       fontSize: 20,
+       width: 119,
+       right: 10,
+       
+  },
+  
   container: {
     flex: 1,
     //backgroundColor: '#349eeb',
